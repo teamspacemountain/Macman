51d0c0f4678cf3bb0bba0d047d5d68c6a577c115
SAM <snaden@macalester.edu>
2014-12-09 22:25:34 -0600

    server serves index.html

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
   console.log("get request to /")
