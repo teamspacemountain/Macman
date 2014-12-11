363f3bf832b40b62ce7acf0a1b2d5d31be966e79
SAM <snaden@macalester.edu>
2014-12-11 00:50:33 -0600

    database setup

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongoskin = require('mongoskin');
+var db = mongoskin.db('mongodb://@localhost:27017/Macman', {safe: true});
+var collections = { scores: db.collection('scores')};
 var logger = require('morgan');
 
 app.use(logger('dev'));
@@ -29,14 +32,31 @@ app.get('/', function (req, res) {
 
 // Handle the put http verb for when we want to save a score
 app.post('/gameover', function(req, res) {
+	var newScores=[100,200,300,400,500];
 	var scoreData = req.body.score;
-	console.log("post request for /gameover handled");
+	console.log("score data is below");
+	console.log(scoreData);
 	console.log(scoreData.score);
-	//res.send(req);
-	// req.collections.images.insert(drawingData, function(error, response){
-	// 	if (error) throw error;
-	// 	res.send(response);
-	// })
+	db.collection('scores').find().toArray(function(err, result) {
+    	if (err) throw err;
+    	//result is an array
+    	console.log(result);
+    	console.log(result[0].scores); //this is the array of current high scores
+    	newScores = [100,200,300,400,500];
+    	// console.log("logging unsorted then sorted");
+    	// console.log(currentHighScores);
+    	// console.log(currentHighScores.sort(function(a, b){return a-b}));  //sorts array in ascending
+    	// var max_of_array = Math.max.apply(Math, currentHighScores);
+    	// console.log("logging max, then min");
+    	// console.log(max_of_array);
+    	// var min_of_array = Math.min.apply(Math, currentHighScores);
+    	// console.log(min_of_array);
+	});
+
+	db.collection('scores').insert({scores: newScores}, function(err, result) {
+    	if (err) throw err;
+    	if (result) console.log('Added!');
+	});
 })
 
 var server = app.listen(8080, function () {
